e24047ac3c1da8268cc84d5e49252edde55c19a7
Luis Gustavo Polio Fernandez <luis.gustavo@qrsof.com>
2025-06-13 23:10:11 -0600

    add app folder to tailwindconfig

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,18 +1,29 @@
 import { useMovies } from "@/presentation/hooks/useMovies";
 import React from "react";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { ActivityIndicator, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
 const HomeScreen = () => {
+  const safeArea = useSafeAreaInsets();
   const { nowPlayingQuery } = useMovies();
 
+  if (nowPlayingQuery.isLoading) {
+    return (
+      <SafeAreaView>
+        <View className="justify-center items-center flex-1">
+          <ActivityIndicator color="pink" size={40} />
+        </View>
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>HomeScreen</Text>
-        <Text>{JSON.stringify(nowPlayingQuery.data)}</Text>
-      </View>
-    </SafeAreaView>
+    <View className="mt-2" style={{ marginTop: safeArea.top }}>
+      <Text className="text-3xl font-bold text-purple-500 px-4 mb-2">Home</Text>
+    </View>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
   content: [
     "./App.tsx",
+    "./app/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
     "./presentation/**/*.{js,jsx,ts,tsx}",
   ],
